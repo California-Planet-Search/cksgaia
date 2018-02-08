712e2f9d7f36dc6a0fa2c38e98a0db46c16b2f0d
BJ Fulton <bjfultn@gmail.com>
2018-02-07 18:16:52 -0800

    adding config setup to easily adjust the tables used for plotting

--- cksgaia/plot/config.py
+++ cksgaia/plot/config.py
@@ -1,1 +1,13 @@
 # put common plotting variables and values here
+import matplotlib
+
+matplotlib.rcParams['font.size'] = 24
+matplotlib.rcParams['figure.figsize'] = (12,10)
+
+
+full_sample = 'johnson17'
+filtered_sample = 'fulton17'
+
+
+print("Making plots with the {} table for the full sample and {} table for the filtered sample."
+      .format(full_sample, filtered_sample))
