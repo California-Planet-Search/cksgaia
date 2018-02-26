734898a77983de2b08614a308abe43518aadb175
BJ Fulton <bjfultn@gmail.com>
2018-02-26 15:36:32 -0800

    working on mass cuts plot

--- bin/run_cksgaia.py
+++ bin/run_cksgaia.py
@@ -203,6 +203,7 @@ class Workflow(object):
         d['insol-contour-anno'] = cksgaia.plot.contour.insol_contour_anno
         d['insol-contour-data'] = cksgaia.plot.contour.insol_contour_data
         d['srad-contour'] = cksgaia.plot.contour.srad_contour
+        d['smass-cuts'] = cksgaia.plot.occur.mass_cuts
 
         self.plot_dict = d
 
