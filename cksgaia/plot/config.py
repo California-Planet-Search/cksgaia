e509a5c5e698329cd18a5b8f714a978f93c47aef
BJ Fulton <bjfultn@gmail.com>
2018-04-27 14:13:29 -0700

    mc

--- cksgaia/plot/config.py
+++ cksgaia/plot/config.py
@@ -19,18 +19,26 @@ print("Making plots with the {} table for the full sample and {} table for the f
       .format(full_sample, filtered_sample))
 
 
-def logscale_rad():
-      pl.semilogx()
-
-      pl.xlabel('Planet Size [Earth radii]')
-
-      pl.xticks([.7, 1.0, 1.3, 1.8, 2.4, 3.5, 4.5, 6, 8.0])
-      # pl.xticks(np.logspace(np.log10(0.7), np.log10(8), 6))
-
-      ax = pl.gca()
-      ax.xaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
-      ax.xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%0.1f'))
-
-      pl.xlim(0.7, 6.0)
+def logscale_rad(axis='y'):
+      if axis == 'x':
+            pl.semilogx()
+            pl.xlabel('Planet Size [Earth radii]')
+            pl.xticks([.7, 1.0, 1.3, 1.8, 2.4, 3.5, 4.5, 6, 8.0])
+            # pl.xticks(np.logspace(np.log10(0.7), np.log10(8), 6))
+            ax = pl.gca()
+            ax.xaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+            ax.xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%0.1f'))
+            pl.xlim(0.7, 6.0)
+      elif axis == 'y':
+            pl.semilogy()
+            pl.ylabel('Planet Size [Earth radii]')
+            pl.yticks([.7, 1.0, 1.3, 1.8, 2.4, 3.5, 4.5, 6, 8.0])
+            # pl.xticks(np.logspace(np.log10(0.7), np.log10(8), 6))
+            ax = pl.gca()
+            ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+            ax.yaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%0.1f'))
+            pl.ylim(0.7, 6.0)
+      else:
+            print("Please specify x or y axis (default: x)")
 
       return ax
